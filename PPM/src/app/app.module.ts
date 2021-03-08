1a886522cbfdbd95963cb9834d8f89339c88829f
vien <mkvien310394@gmail.com>
2021-03-08 23:43:54 +0700

    WebApp
    Angular

---
+++ PPM/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PatientComponent } from 'src/component-patient/patient.component';
+import { LoginComponent } from 'src/component-login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    PatientComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
